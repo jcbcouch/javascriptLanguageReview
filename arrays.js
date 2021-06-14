6487ff8b82d7c312144205c753ecd2fafb63b551
jcbcouch <jcbcouch@gmail.com>
2021-06-14 01:29:39 -0700

    more array hofs

--- arrays.js
+++ arrays.js
@@ -7,6 +7,7 @@ arrayTest = Array.isArray(myArray);  // true
 zerothIndex = myArray[0];  // true (arrays are zero based)
 myArray[0] = false;  // change an index
 getIndex = myArray.indexOf('hello');  // 4
+hasHello = myArray.includes('hello');  // true
 myArray.push('last');  // add new last index
 myArray.pop(); // remove last index
 myArray.unshift('first'); // new first index, previous first becomes 2nd...
@@ -17,7 +18,6 @@ numbers.splice(7,2);  // remove two indexes starting at index 7
 numbers.reverse();  // reverse the array
 numbers = numbers.concat([33,45,73,78,17,62,77,999,333,423]);  // push an array to the back
 numbers.sort();  // sort by first digit(number or letter)
-
 
 
 const companies = [
@@ -41,8 +41,6 @@ companies.forEach((c, i, companies) => {
 });
 
 
-
-
 // filter filter filter filter filter filter filter filter
 // keep if true, filter out if false
 const retail = companies.filter((c) => {
@@ -56,7 +54,8 @@ const retail2 = companies.filter((c) => c.category === 'Retail');
 
 
 // map map map map map map map map map map map
-const companyNames = companies.map((c) => c.name);
+// build a new array however you want
+const companyNames = companies.map((c) => `company name: ${c.name}`);
 
 
 // sort sort sort sort sort sort sort sort sort sort sort sort
@@ -71,6 +70,37 @@ const startDateSorted2 = companies.sort((c1, c2) => c1.start > c2.start ? 1 : -1
 const startDateSorted3 = companies.sort((a, b) => a - b);
 
 
+// reduce reduce reduce reduce reduce reduce reduce reduce reduce
+const totalYears = companies.reduce((acc, company) => {
+    acc += (company.end - company.start);
+    return acc
+}, 0);
 
-console.log(startDateSorted3);
+const totalYears2 = companies.reduce((acc, c) => acc + (c.end - c.start), 0);
+const averageYears = companies.reduce((acc, c) => acc + (c.end - c.start), 0) / companies.length;
 
+
+// find find find find find find find find find find find find find 
+// returns the first one that matches true
+const companyOne = companies.find((c) => {
+    return c.name === 'Company One'
+});
+
+const companyOne2 = companies.find((c) => c.name === 'Company One');
+
+
+// some some some some some some some some some some some some some 
+// returns true if anything in array meets condition
+const lastedLong = companies.some((c) => {
+    return c.end - c.start > 10;
+});
+
+const lastedLong2 = companies.some((c) => c.end - c.start > 25);
+
+
+// every every every every every every every every every every every 
+// returns true if everything in the array meets condition
+const lastedFiveYears = companies.every((c) => c.end - c.start >= 5);
+
+
+console.log(lastedFiveYears);
